81866cbc931fab073b139a3a13495e26760899ad
kanoyami <soulmaker@msn.cn>
2015-05-23 18:56:32 +0800

    HTML页面分拣

--- set.php
+++ set.php
@@ -5,6 +5,7 @@ $QQ = $_POST['QQ'];
 $Phone = $_POST['Phone'];
 $userinfo = $_POST['UserInfo'];
 $sex = $_POST['sex'];
+$pageID = $_POST['pageID'];
 $con = mysql_connect("localhost","root","sw960602");
 if (!$con)
   {
@@ -15,11 +16,11 @@ if (!$con)
 mysql_select_db("studentinfo", $con);
 mysql_query("set names utf8");
 
-mysql_query("CREATE TABLE IF NOT EXISTS studentunion(IDnum VARCHAR(22) NOT NULL,
+mysql_query("CREATE TABLE IF NOT EXISTS $pageID(IDnum VARCHAR(22) NOT NULL,
 													 sex TINYTEXT,name TEXT,QQ VARCHAR(15),
 													 phone VARCHAR(24) NOT NULL,info LONGTEXT)");
 
-$sql="INSERT INTO studentunion (IDnum,name,QQ,phone,info,sex)
+$sql="INSERT INTO $pageID (IDnum,name,QQ,phone,info,sex)
 VALUES
 ('$ID','$name','$QQ','$Phone','$userinfo','$sex')";
 
